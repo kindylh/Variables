cb8798bc8bab26c47d5f510ca23d5665bc1e36dd
100138808 <kindyl.hogan245@gmail.com>
2023-09-11 09:05:27 -0500

    Variables

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+//byte byteOne = 127;
+        System.out.println("///////////////////////////////");
+        System.out.println("*Kindyl Hogan         07/18/02*");
+        System.out.println("*                             *");
+        System.out.println("*                             *");
+        System.out.println("*                             *");
+    }
+}
