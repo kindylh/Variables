08694e33460c033a9901564e02717f3d2060adef
100138808 <kindyl.hogan245@gmail.com>
2023-09-15 08:26:50 -0500

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,45 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Variables
+
+public class Main
 {
-    public static void main ( String[] args )
+
+    public static void main (String[] args )
     {
 //define 1 variable of each of the
 //following data types
-//byte short int long
-//float double
-//char boolean String
-//integer variables
-//byte byteOne = 127;
-        System.out.println("///////////////////////////////");
+        short shortOne = -32123;
+        int intOne = 90877;
+        long longOne = 999999999;
+
+        //byte short int long
+        //float double
+        float floatOne = 12;
+        double doubleOne = 923.234;
+
+        //char boolean String
+        char charOne = 'A';
+        // boolean booleanOne = 'true';
+        //stringOne = 'hello world';
+
+        //integer variables
+        byte byteOne = 127;
+        System.out.println("/////////////////////////////");
         System.out.println("*Kindyl Hogan         07/18/02*");
         System.out.println("*                             *");
+        System.out.println("*        integer types         *");
         System.out.println("*                             *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+        System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+        System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+        System.out.println("*64 bit - longOne = "+longOne+"\t*");
         System.out.println("*                             *");
+        System.out.println("*         real types          *");
+        System.out.println("*                             *");
+        System.out.println("*32 bit - floatOne ="+floatOne+"\t*");
+        System.out.println("*64 bit - doubleOne ="+doubleOne+"\t*");
+        System.out.println("*      other integer types    *");
+        System.out.println("*16 bit - charOne ="+charOne+"\t\t*");
+        System.out.println("*                              *");
+        System.out.println("*        other types            *");
+        System.out.println("*//////////////////////////*");
     }
 }
